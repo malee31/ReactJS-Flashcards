91d9b58d79e45c9ebc29b5c211c19d128900217e
Marvin Lee <43216761+malee31@users.noreply.github.com>
2020-03-29 15:23:52 -0700

    Added inputs to add to the set as state of the top level Route component

--- src/components/Creator.js
+++ src/components/Creator.js
@@ -1,5 +1,25 @@
 import React from 'react';
 
-export default() => {
-	return(<p>Creator.js</p>)
+export default(props) => {
+	return(
+		<>
+			<p>Create your Flashcard Set</p>
+			<form onSubmit={props.addWord}>
+				{props.wordSets.map(set => {
+					console.log(set);
+					return(
+						<div className="set" key={set.front + set.back}>
+							<input type="text" value={set.front} readOnly></input>
+							<input type="text" value={set.back} readOnly></input>
+						</div>
+					);
+				})}
+				<div id="newCard">
+					<input type="text" placeholder="New Word" value={props.pair["front"]} onChange={e => {props.change("front", e);}}></input>
+					<input type="text" placeholder="New Definition" value={props.pair["back"]} onChange={e => {props.change("back", e);}}></input>
+				</div>
+				<input type="submit" value="Add"></input>
+			</form>
+		</>
+	);
 };
--- src/components/Route.js
+++ src/components/Route.js
@@ -3,12 +3,45 @@ import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import Home from './Home.js';
 import Creator from './Creator.js';
 import Flashcards from './Flashcards.js';
-import WordBank from './WordBank.js'
 
-export default() => {
-	//Optional Nav can be added before <Switch>
-	return(
-		<WordBank>
+export default class WordBank extends React.Component
+{
+	constructor(props)
+	{
+		super(props);
+		this.state = {
+			words: [], //type Array.{front: string, back: string}
+			currentPair: {"front": "", "back": ""}
+		};
+		this.setCurrentPair = this.setCurrentPair.bind(this);
+		this.addCurrentPair = this.addCurrentPair.bind(this);
+	}
+
+	setCurrentPair(index, e)
+	{
+		e.preventDefault();
+		var newPair = Object.assign({}, this.state.currentPair);
+		newPair[index] = e.target.value;
+		this.setState({
+			currentPair: newPair
+		});
+	}
+
+	addCurrentPair(e)
+	{
+		e.preventDefault();
+		if(this.state.currentPair.front == "" && this.state.currentPair.back == "") return;
+		var newWordSet = this.state.words.slice();
+		newWordSet.push(this.state.currentPair);
+		this.setState({
+			words: newWordSet,
+			currentPair: {"front": "", "back": ""}
+		});
+	}
+
+	render()
+	{
+		return(
 			<Router>
 				<Switch>
 					<Route exact path={["/", "/home"]}>
@@ -16,13 +49,13 @@ export default() => {
 						<Link to="/cards">Cards</Link>
 					</Route>
 					<Route exact path="/create">
-						<Creator/>
+						<Creator wordSets={this.state.words} addWord={this.addCurrentPair} pair={this.state.currentPair} change={this.setCurrentPair}/>
 					</Route>
 					<Route exact path="/cards">
 						<Flashcards/>
 					</Route>
 				</Switch>
 			</Router>
-		</WordBank>
-	);
+		);
+	}
 };
