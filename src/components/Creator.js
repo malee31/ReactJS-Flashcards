2ece0dcdcd4f98fa7980ceecde54d29526d70d8a
Marvin Lee <43216761+malee31@users.noreply.github.com>
2020-04-02 21:21:06 -0700

    Added title and desc. Styled /create more

--- src/components/Creator.js
+++ src/components/Creator.js
@@ -1,23 +1,26 @@
 import React from 'react';
+import '../css/Creator.css';
 
 export default(props) => {
 	return(
 		<>
 			<p>Create your Flashcard Set</p>
 			<form onSubmit={props.addWord}>
-				{props.wordSets.map(set => {
-					return(
-						<div className="set" key={set.front + set.back}>
-							<input type="text" value={set.front} readOnly></input>
-							<input type="text" value={set.back} readOnly></input>
-						</div>
-					);
-				})}
-				<div id="newCard">
-					<input type="text" placeholder="New Word" value={props.pair["front"]} onChange={e => {props.change("front", e);}}></input>
-					<input type="text" placeholder="New Definition" value={props.pair["back"]} onChange={e => {props.change("back", e);}}></input>
+				<div id="wordsets">
+					{props.wordSets.map(set => {
+						return(
+							<div className="set" key={set.front + set.back}>
+								<input className="word" type="text" value={set.front} readOnly></input>
+								<input className="define" type="text" value={set.back} readOnly></input>
+							</div>
+						);
+					})}
 				</div>
-				<input type="submit" value="Add"></input>
+				<div id="newCard">
+					<input className="word" type="text" placeholder="New Word" value={props.pair["front"]} onChange={e => {props.change("front", e);}}></input>
+					<input className="define" type="text" placeholder="New Definition" value={props.pair["back"]} onChange={e => {props.change("back", e);}}></input>
+				</div>
+				<input type="submit" value="Add Card"></input>
 			</form>
 		</>
 	);
