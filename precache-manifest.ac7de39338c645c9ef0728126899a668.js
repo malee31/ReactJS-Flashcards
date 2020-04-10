8af50397ed709ffc724c17535d7b675ce0aad2fd
Marvin Lee <43216761+malee31@users.noreply.github.com>
2020-04-10 12:37:59 -0700

    Updates

---
+++ precache-manifest.ac7de39338c645c9ef0728126899a668.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95dcf7584a94e0ddac1489d014238fcf",
+    "url": "/ReactJS-Flashcards/index.html"
+  },
+  {
+    "revision": "072ff4f9876bbaa36a2c",
+    "url": "/ReactJS-Flashcards/static/css/main.915aa12a.chunk.css"
+  },
+  {
+    "revision": "30462adb8af8d1feba02",
+    "url": "/ReactJS-Flashcards/static/js/2.c3f01028.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactJS-Flashcards/static/js/2.c3f01028.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "072ff4f9876bbaa36a2c",
+    "url": "/ReactJS-Flashcards/static/js/main.25edd9e6.chunk.js"
+  },
+  {
+    "revision": "a731948f64188fb7625d",
+    "url": "/ReactJS-Flashcards/static/js/runtime-main.1d3622b9.js"
+  },
+  {
+    "revision": "5d3b79930b0bd82a8ee57cdf59ca1d50",
+    "url": "/ReactJS-Flashcards/static/media/HomeIcon.5d3b7993.svg"
+  }
+]);
